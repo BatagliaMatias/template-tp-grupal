cf5a9fe087a3737811deddd853800d15a2e8032f
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 13:54:36 -0300

    Nuevo juego simple TddGame. Con algunos tests, sin diseño.

--- src/test/java/ar/fiuba/tdd/tp/MainTests.java
+++ src/test/java/ar/fiuba/tdd/tp/MainTests.java
@@ -10,4 +10,22 @@ public class MainTests {
     public void dummy() {
         assertEquals(0, 0);
     }
+
+    @Test
+    public void initialGameTDD() {
+        TddGame myGame = new TddGame();
+       String out = myGame.processInput("nothing");
+
+        assertEquals(out,"The name of this Subject is ...");
+    }
+
+    @Test
+    public void gamingTDD() {
+        TddGame myGame = new TddGame();
+        String out = myGame.processInput("nothing");
+        assertEquals(out,"The name of this Subject is ...");
+        String secondOut = myGame.processInput("TDD");
+        assertEquals(secondOut,"YES. YOU WIN. THE GAME START AGAIN...");
+    }
+
 }
