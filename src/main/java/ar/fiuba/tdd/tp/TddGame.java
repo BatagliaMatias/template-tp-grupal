d375ddca53759677989aab9968ebf37ab5a94a1b
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 13:56:10 -0300

    Nuevo juego simple TddGame. Con algunos tests, sin diseño.

---
+++ src/main/java/ar/fiuba/tdd/tp/TddGame.java
@@ -0,0 +1,28 @@
+package ar.fiuba.tdd.tp;
+
+public class TddGame {
+    private static final int BEGIN = 0;
+    private static final int SEND = 1;
+
+    private int state = BEGIN;
+
+    public String processInput(String theInput) {
+        String output = null;
+
+        if (state == BEGIN) {
+            output = "The name of this Subject is ...";
+            state = SEND;
+        }
+        else if (state == SEND) {
+            if (theInput.equalsIgnoreCase("TDD")) {
+                output = "YES. YOU WIN. THE GAME START AGAIN...";
+                state = BEGIN;
+            } else {
+                output = "NO. TRY AGAIN";
+            }
+        }
+     return output;
+
+    }
+
+}
