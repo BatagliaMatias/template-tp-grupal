67f28879a7e301024199a08328bb8ca0ce171537
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-18 20:58:48 -0300

    parametros del servidor

--- src/main/java/ar/fiuba/tdd/tp/Server.java
+++ src/main/java/ar/fiuba/tdd/tp/Server.java
@@ -1,7 +1,10 @@
 package ar.fiuba.tdd.tp;
 
 public class Server {
-    public static void main(String[] args){
-        //Code for Server
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("To use: java Server <port number>");
+            System.exit(1);
+        }
     }
 }
