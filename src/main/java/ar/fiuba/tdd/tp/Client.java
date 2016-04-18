ffac6ef4f84a2e00ca95e98701dd69918d32deb0
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 23:26:28 -0300

    Agregando clases Client y Server vacias

---
+++ src/main/java/ar/fiuba/tdd/tp/Client.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.tp;
+
+public class Client {
+    public static void main(String[] args){
+        //Code for Client
+    }
+}
---
+++ src/main/java/ar/fiuba/tdd/tp/Server.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.tp;
+
+public class Server {
+    public static void main(String[] args){
+        //Code for Server
+    }
+}
