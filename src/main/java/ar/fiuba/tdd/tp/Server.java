9ead74c4e3ced56946fd209ebfc11253a7f17001
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 23:35:24 -0300

    Server adding functionality

--- src/main/java/ar/fiuba/tdd/tp/Server.java
+++ src/main/java/ar/fiuba/tdd/tp/Server.java
@@ -2,6 +2,11 @@ package ar.fiuba.tdd.tp;
 
 public class Server {
     public static void main(String[] args){
-        //Code for Server
+        if (args.length != 1) {
+            System.err.println("Usage: > java Server <port> ->init a Server in this port");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        System.out.print(port);
     }
 }
