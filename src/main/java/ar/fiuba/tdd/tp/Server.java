65c9b48064242045e47d42233c17a2c6fa71baac
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-17 23:57:30 -0300

    chekstyle

--- src/main/java/ar/fiuba/tdd/tp/Server.java
+++ src/main/java/ar/fiuba/tdd/tp/Server.java
@@ -4,26 +4,22 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Usage: > java Server <port> ->init a Server in this port");
             System.exit(1);
         }
-
         int port = Integer.parseInt(args[0]);
         System.out.print(port);
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             Socket clientSocket = serverSocket.accept();
-            PrintWriter out =
-                    new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             String inputLine, outputLine;
 
-            // Conect with client
             TddGame game = new TddGame();
             outputLine = game.processInput(null);
             out.println(outputLine);
@@ -32,13 +28,12 @@ public class Server {
                 outputLine = game.processInput(inputLine);
                 out.println(outputLine);
                 if (outputLine.equals("YES. YOU WIN. THE GAME START AGAIN..."))
-                    break;//FInish conection when client won
+                    break;
             }
+
         } catch (IOException e) {
-            System.out.println("Error to conect");
+            System.out.println("Error");
             System.out.println(e.getMessage());
         }
-
-
     }
 }
