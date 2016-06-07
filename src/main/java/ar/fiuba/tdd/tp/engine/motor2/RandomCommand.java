d33cfd4ad9a036a7241435ffb64b73bb730b2c28
Matias Bataglia <matubat91@gmail.com>
2016-06-07 20:47:55 -0300

    Cambio comando random para poder activar y desactivar las opciones

--- src/main/java/ar/fiuba/tdd/tp/engine/motor2/RandomCommand.java
+++ src/main/java/ar/fiuba/tdd/tp/engine/motor2/RandomCommand.java
@@ -1,22 +1,38 @@
 package ar.fiuba.tdd.tp.engine.motor2;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RandomCommand extends Command {
-    private ArrayList<Command> options;
+    private HashMap<Command,Boolean> commands;
     public RandomCommand(String name) {
         super(name);
-        options  = new ArrayList<>();
+        commands = new HashMap<>();
     }
 
-    public void addOptionCommand(Command option) {
-        options.add(option);
+    public void addOptionCommand(Command command) {
+        commands.put(command,true);
+    }
+
+    public void activeCommand(Command command) {
+        commands.put(command,true);
+    }
+
+    public void desactiveCommand(Command command) {
+        commands.put(command,false);
+    }
+
+
+    private List<Command> getActiveCommands() {
+        return commands.entrySet().stream()
+                .filter(entry -> entry.getValue())
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
     }
 
     private Command getRandomOption() {
-        int optionNumber = new Random().nextInt(options.size());
-        return options.get(optionNumber);
+        int optionNumber = new Random().nextInt(getActiveCommands().size());
+        return getActiveCommands().get(optionNumber);
     }
 
     @Override
